3df39f8d721f8cf062e1f82ce5e40cf1f764e197
EvilisGlenio <glenio.developer@gmail.com>
2026-07-24 10:56:57 -0300

    feat: add class-transformer and class-validator dependencies; enhance user DTO validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
-  name: string;
-  email: string;
-  passwordHash: string;
+  @IsString({
+    message: 'Name must be a string',
+  })
+  @IsNotEmpty()
+  name!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @IsNotEmpty()
+  @MinLength(8, {
+    message: 'Password hash must be at least 8 characters long',
+  })
+  passwordHash!: string;
 }
